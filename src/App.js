02e27256bd5562907cf1b6c5ff0fed356de9e916
Emran12 <emran6452@gmail.com>
2022-09-29 23:09:45 +0600

    landing page added for mobile device

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <div>
-      <h1>React JS</h1>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import landingPage from "../../images/LandingPage.svg";
+import birthdayCap from "../../images/birthdayCap.png";
+import "./Home.css";
 
 const Home = () => {
-  return <div>This is Home</div>;
+  return (
+    <div>
+      <div>
+        <h1 className="title">
+          Imagine if<span className="snapchat">Snapchat</span> had events.
+        </h1>
+        <p className="subTitle">
+          Easily host and share events with your friends across any social
+          media.
+        </p>
+      </div>
+      <div>
+        <img className="landingPage" src={landingPage} alt=""></img>
+        <Link>
+          {/* <img className="createEvent" src={birthdayCap} alt=""></img> */}
+        </Link>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Home;
